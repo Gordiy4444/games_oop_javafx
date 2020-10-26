454f3f9ee8316ad778b5d09a6f60ccdc099432a5
Gordiy4444 <vazhnoe01@gmail.com>
2020-10-26 12:49:44 +0400

    1

--- chess/src/test/java/ru/job4j/chess/firuges/black/BishopBlackTest.java
+++ chess/src/test/java/ru/job4j/chess/firuges/black/BishopBlackTest.java
@@ -4,11 +4,15 @@ import junit.framework.TestCase;
 import org.junit.Test;
 import ru.job4j.chess.firuges.Cell;
 
+import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
 public class BishopBlackTest {
     @Test
     public void whenBishopBlackTest(){
-        BishopBlack sl = new  BishopBlack(Cell.A1);
+        BishopBlack sl = new  BishopBlack(Cell.E3);
+        Cell[] cells = sl.way(Cell.G5);
+        Cell[] dells = new Cell[]{Cell.F4,Cell.G5};
+        assertThat(cells, is(dells));
     }
 }
