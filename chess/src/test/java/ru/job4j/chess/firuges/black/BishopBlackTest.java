b6bcbcd3a20a8384b60827df4e8e02e41ab54ab7
Gordiy4444 <vazhnoe01@gmail.com>
2020-11-02 14:27:17 +0400

    1

--- chess/src/test/java/ru/job4j/chess/firuges/black/BishopBlackTest.java
+++ chess/src/test/java/ru/job4j/chess/firuges/black/BishopBlackTest.java
@@ -2,6 +2,7 @@ package ru.job4j.chess.firuges.black;
 
 import junit.framework.TestCase;
 import org.junit.Test;
+import ru.job4j.chess.ImpossibleMoveException;
 import ru.job4j.chess.firuges.Cell;
 
 import static org.hamcrest.Matchers.is;
@@ -9,7 +10,7 @@ import static org.junit.Assert.assertThat;
 
 public class BishopBlackTest {
     @Test
-    public void whenBishopBlackTest(){
+    public void whenBishopBlackTest() throws ImpossibleMoveException {
         BishopBlack sl = new  BishopBlack(Cell.E3);
         Cell[] cells = sl.way(Cell.G5);
         Cell[] dells = new Cell[]{Cell.F4,Cell.G5};
